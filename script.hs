ee07de6c9d1274709edc2750301eafbd6ddbe5d5
philipp.jenni <philipp.jenni@unic.com>
2025-05-23 08:42:03 +0200

    Programming in Haskell - first experiments to create a script.hs

---
+++ script.hs
@@ -0,0 +1,16 @@
+double x = x + x
+quadruple x = double (double x)
+
+factorial n = product [1..n]
+
+average ns = sum ns `div` length ns
+
+n = a `div`length xs
+  where
+    a = 10
+    xs = [1,2,3,4,5]
+
+lastJep xs = xs !! (length xs - 1)
+
+initVersion1 xs = take (length xs - 1) xs
+initVersion2 xs = reverse (tail (reverse xs))
